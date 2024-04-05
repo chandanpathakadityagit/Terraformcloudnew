c7566ac57b964b9d97eec6f6a4208098b7e8e4b0
chandanpathakadityagit <165925379+chandanpathakadityagit@users.noreply.github.com>
2024-04-05 20:27:27 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "google_compute_instance" "example" {
+  name         = "test"
+  machine_type = "e2-micro"
+  zone         = "us-central1-b"
+  allow_stopping_for_update = "true"
+ 
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+    }
+  }
+ 
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
